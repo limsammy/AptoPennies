ea55c15e4291cab2c595ff7a5a2fcf229d38adfa
limsammy1@gmail.com <limsammy1@gmail.com>
2018-03-13 08:39:44 -0600

    Added bonus step

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ function moneySplitter(total, count) {
   console.log(splitThree(total));
   console.log(validate(splitThree(total), total));
 
+  total = total / count;
+
   validate(splitThree(total), total);
 };
 
