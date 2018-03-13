ee082c4f856fe10ab49140f5ecc0fe3101978931
limsammy1@gmail.com <limsammy1@gmail.com>
2018-03-13 08:12:34 -0600

    Got splitThree working

---
+++ app.js
@@ -0,0 +1,14 @@
+function moneySplitter(total) {
+
+}
+
+function splitThree(integer) {
+  let results = [];
+  let result = integer / 3;
+  for (var i=0; i<3; i++) {
+    results.push(result);
+  }
+  return results;
+}
+
+splitThree(100);
