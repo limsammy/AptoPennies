890dc2cebe29f67f7ca62a8d85174c5becd8d8e3
limsammy1@gmail.com <limsammy1@gmail.com>
2018-03-13 08:14:57 -0600

    Wrote splitThree

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function moneySplitter(total) {
-
+  splitThree(total);
 }
 
 function splitThree(integer) {
   let results = [];
   let result = integer / 3;
+  result = Math.round(result * 100) / 100;
   for (var i=0; i<3; i++) {
     results.push(result);
-  }
+  };
+  // console.log(results);
   return results;
-}
-
-splitThree(100);
+};
